8613200e34dd76293e572e99f583d0bc05fd2409
jamisongrant <jgbatchelor@gmail.com>
2017-01-14 19:44:53 -0700

    exercise 2 ruby the hard way

---
+++ rthw2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so I can read my program later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have a code like this." # and the comment after is ignored
+
+# I can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
