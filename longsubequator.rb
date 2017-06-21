532ea7a23a197f03376bf46caa181d10720fa30e
jamisongrant <jgbatchelor@gmail.com>
2017-06-20 18:42:43 -0600

    method to put out longest input of 2 given

---
+++ longsubequator.rb
@@ -0,0 +1,55 @@
+str1 = "the quick brown fox jumps over the lazy dog"
+str2 = "something something quick dog"
+
+# def longsubsequator(str1,str2)
+#   substr = str.split(" ")
+#   substr.sort! { |a, b| b.length <=> a.length }
+#   substr[0]
+#   return substr[0]
+# end
+
+# puts longsubsequator(str1)
+
+# def longest(string)
+#   string.split(" ").max_by(&:length)
+# end
+
+# puts longsubsequator(str1)
+
+# def longsubsequator str1, str2
+#   longest_word = ''
+#   str = str.split(" ")
+#   str.each do |word|
+#     longest_word = word if longest_word.length
+#   end
+#   puts longest_word
+# end
+
+# puts longsubsequator(str1,str2)
+
+
+
+# def longest_word(array)
+#   longest = ""
+#   num = -
+
+#   array.each do |word|
+#     if word.length > num
+#       longest = word
+#     end
+#   end
+  
+#   longest
+# end
+
+# def compare_arrays(arr_1, arr_2)
+#   arr = []
+#   arr_1.each do |x|
+#     arr_2.each do |y|
+#       arr << x if x == y
+#     end
+#   end
+#   p longest_word(arr)
+# end
+
+# compare_arrays(str1.split(" "),str2.split(" "))
